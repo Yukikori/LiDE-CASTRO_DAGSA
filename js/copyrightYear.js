ca35f60bf15b3ced0ae4363dd972d4d53a7e5f84
JDagsa-Pisay <b2026jrvdagsa@pshs.edu.ph>
2022-12-04 20:52:25 +0800

    Simplify script for copyrightYear, properly link js

--- js/copyrightYear.js
+++ js/copyrightYear.js
@@ -1,9 +1,10 @@
 var year = new Date().getFullYear();
+var copyrightYear = document.getElementById("copyrightYear")
 
 if (year != 2022) {
-    document.getElementById("copyrightYear").textContent = `2022-${year}`
+    copyrightYear.innerText = `2022-${year}`
     console.log(`Current year not 2022, used 2022-${year}`)
 } else {
-    document.getElementById("copyrightYear").textContent = `2022`
+    copyrightYear.innerText = `2022`
     console.log(`Current year is 2022`)
 }
