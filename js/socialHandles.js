d9d6635ac11378b88026a74ba2d70b1170a39bf5
JDagsa-Pisay <b2026jrvdagsa@pshs.edu.ph>
2022-12-15 11:18:02 +0800

    Added new pages, used svg sprite for icons, more js

---
+++ js/socialHandles.js
@@ -0,0 +1,15 @@
+// Work in progress
+
+// gets file path
+var location = window.location.pathname;
+
+// conditions for specific webpages - substitute social media hrefs in anchor tags
+if (location.includes("beginnings-of-mobile-gaming")) {
+
+} else if (location.includes("classic-mobile-genres")) {
+
+} else if (location.includes("newer-mobile-genres")) {
+
+} else if (location.includes("mobile-gaming-today")) {
+
+};
