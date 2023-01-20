6c4a64a7798fb5e6ac8a42ec400e175498836de2
JDagsa-Pisay <b2026jrvdagsa@pshs.edu.ph>
2023-01-20 08:41:30 +0800

    Add heroicons for some elements, improved responsiveness

--- js/openDialog.js
+++ js/openDialog.js
@@ -2,15 +2,27 @@ var openButton = document.getElementById("open");
 var dialog = document.getElementById("dialog");
 var closeButton = document.getElementById("close");
 var overlay = document.getElementById("overlay");
+var bodyDoc = document.body;
 
 // show the overlay and the dialog
 openButton.addEventListener("click", function () {
-  dialog.classList.remove("invisible");
-  overlay.classList.remove("invisible");
+  dialog.classList.toggle("invisible");
+  overlay.classList.toggle("invisible");
+  bodyDoc.style.top = `-${window.scrollY}px`;
+  bodyDoc.classList.toggle("absolute");
+  bodyDoc.classList.toggle("fixed");
+  bodyDoc.classList.toggle("overflow-hidden");
 });
 
 // hide the overlay and the dialog
 closeButton.addEventListener("click", function () {
-  dialog.classList.add("invisible");
-  overlay.classList.add("invisible");
+  dialog.classList.toggle("invisible");
+  overlay.classList.toggle("invisible");
+  bodyDoc.classList.toggle("fixed");
+  bodyDoc.classList.toggle("absolute");
+  bodyDoc.classList.toggle("overflow-hidden");
+
+  const scrollY = bodyDoc.style.top;
+  bodyDoc.style.top = "";
+  window.scrollTo(0, parseInt(scrollY || "0") * -1);
 });
