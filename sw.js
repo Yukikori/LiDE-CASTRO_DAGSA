f8804d5df5759c00b97fb06e6821a9c6c16267e8
JDagsa-Pisay <b2026jrvdagsa@pshs.edu.ph>
2023-01-22 23:14:55 +0800

    Rewrite sw.js to fix caching

--- sw.js
+++ sw.js
@@ -1,84 +1,72 @@
-self.addEventListener("install", function (event) {
-  event.waitUntil(caches.open("app")).then(function (cache) {
-    let coreAssets = [
-      "./css/style.css",
-      "./js/copyrightYear.js",
-      "./js/openDialog.js",
-      "./js/socialHandles.js",
-      "./js/handle-yt-offline.js",
-      "./images/AngryBirds-bg.jpg",
-      "./images/Bejeweled-bg.jpg",
-      "./images/CODM-bg.jpg",
-      "./images/Genshin-bg.jpg",
-      "./images/bootstrap-icons.svg",
-      "./images/angry-birds.mp3",
-      "./images/bejeweled3-classic.mp3",
-      "./images/codm-wild-west.mp3",
-      "./images/sanctuary-of-surasthana-extended.mp3",
-      "./fonts/PressStart2P-Regular.ttf",
-      "./beginnings-of-mobile-gaming/index.html",
-      "./classic-mobile-genres/index.html",
-      "./newer-mobile-genres/index.html",
-      "./mobile-gaming-today/index.html",
-      "./favicon-light.ico",
-      "./favicon-dark.ico",
-      "./favicon-light.png",
-      "./favicon-dark.png",
-      "./favicon.ico",
-    ];
-    for (let asset of coreAssets) {
-      cache.add(new Request(asset));
-    }
-    return cache;
-  });
+const cacheName = 'app';
+
+this.addEventListener('install', function (event) {
+  console.log('installing...');
+
+  let coreAssets = [
+    './',
+    './beginnings-of-mobile-gaming/',
+    './classic-mobile-genres/',
+    './newer-mobile-genres/',
+    './mobile-gaming-today/',
+    './css/style.css',
+    './js/copyrightYear.js',
+    './js/handle-yt-offline.js',
+    './js/openDialog.js',
+    './js/socialHandles.js',
+    './favicon-light.ico',
+    './favicon-dark.ico',
+    './favicon.ico',
+    './favicon-light.png',
+    './favicon-dark.png',
+    './safari-pinned-tab-icon.svg',
+    './audio/angry-birds.mp3',
+    './audio/bejeweled3-classic.mp3',
+    './audio/codm-wild-west.mp3',
+    './audio/sanctuary-of-surasthana-extended.mp3',
+  ];
+
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(coreAssets);
+    })
+  );
 });
 
-self.addEventListener("fetch", function (event) {
-  let request = event.request;
+this.addEventListener('fetch', function (event) {
+  console.log('fetch some data!');
 
-  if (
-    event.request.cache === "only-if-cached" &&
-    event.request.mode !== "same-origin"
-  ) {
+  if (event.request.headers.has('range')) {
     return;
   }
 
-  if (request.headers.get("Accept").includes("text/html")) {
-    event.respondWith(
-      fetch(request)
-        .then(function (response) {
-          return response;
-        })
-        .catch(function (error) {
-          return caches.match(request).then(function (response) {
+  event.respondWith(
+    caches.open(cacheName).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return (
+          response ||
+          fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone());
             return response;
-          });
+          })
+        );
+      });
+    })
+  );
+});
+
+this.addEventListener('activate', function activator(event) {
+  console.log('activate!');
+
+  caches.keys().then(function (keys) {
+    return Promise.all(
+      keys
+        .filter(function (key) {
+          return key.indexOf(cacheName) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
         })
     );
-  }
-
-  if (
-    request.headers.get("Accept").includes("text/css") ||
-    request.headers.get("Accept").includes("text/javascript")
-  ) {
-    caches.match(request).then(function (response) {
-      return (
-        response ||
-        fetch(request).then(function (response) {
-          return response;
-        })
-      );
-    });
-  }
-
-  if (request.headers.get("Accept").includes("image")) {
-    caches.match(request).then(function (response) {
-      return (
-        response ||
-        fetch(request).then(function (response) {
-          return response;
-        })
-      );
-    });
-  }
+  });
 });
